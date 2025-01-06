c93e4c23e0abeb928671263052d4f94364b4645d
santiago arteche <santiarteche@hotmail.com>
2025-01-06 02:24:23 -0300

    auth.middleware strategy changed

--- src/domain/middlewares/auth.middleware.ts
+++ src/domain/middlewares/auth.middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { JWT } from "../../infraestructure/config/jwt";
 import { userModel } from "../../infraestructure/data/mongo-db/models/user.model";
+import { JsonWebTokenError } from "jsonwebtoken";
 
 export class AuthMiddleware {
   static validateJWT = async (
@@ -9,32 +10,35 @@ export class AuthMiddleware {
     next: NextFunction
   ) => {
     const headers = request.headers;
-    const cookie = headers.cookie;
 
-    const findAuthToken = cookie
-      ?.split(";")
-      .find((cookie) => cookie.trim().startsWith("auth_token"));
+    const token = headers["x-auth"];
 
-    if (!findAuthToken) {
+    if (!token) {
       return response
         .status(404)
         .json({ msg: "You need to login before use the API" });
     }
 
-    const token = findAuthToken.split("=")[1];
+    try {
+      const verifyToken = await JWT.decode(token.toString());
 
-    const verifyToken = await JWT.decode(token);
+      if (!verifyToken) {
+        return response.status(400).json({ msg: "Invalid Token" });
+      }
 
-    if (!verifyToken) {
-      return response.status(400).json({ msg: "Invalid Token" });
+      const findUser = await userModel.findOne({ email: verifyToken.email });
+
+      if (!findUser) {
+        return response.status(400).json({ msg: "Invalid Token" });
+      }
+
+      next();
+    } catch (error) {
+      if (error instanceof JsonWebTokenError) {
+        return response.status(500).json({ msg: "Error validating token" });
+      } else {
+        return response.status(500).json({ msg: "Internal Server Error" });
+      }
     }
-
-    const findUser = await userModel.findOne({ email: verifyToken.email });
-
-    if (!findUser) {
-      return response.status(400).json({ msg: "Invalid Token" });
-    }
-
-    next();
   };
 }
